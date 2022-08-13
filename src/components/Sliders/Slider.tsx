89308f3eb15ac1d3bbe26bb91e5404c2623d6720
FORTRIX <fortrix07@gmail.com>
2022-08-13 15:18:52 +0300

    Make links in slider, fixed empty shopcart

--- src/components/Sliders/Slider.tsx
+++ src/components/Sliders/Slider.tsx
@@ -19,11 +19,11 @@ type SlideImagesType = {
 }
 
 const slideImages: SlideImagesType[] = [
-  {id: 1, imgUrl: 'http://localhost:3000/img/background_1.jpg', alt: 'img1', urlTo: '/product'},
-  {id: 2, imgUrl: 'http://localhost:3000/img/background_2.jpg', alt: 'img2', urlTo: '/product'},
-  {id: 3, imgUrl: 'http://localhost:3000/img/background_3.jpg', alt: 'img3', urlTo: '/product'},
-  {id: 4, imgUrl: 'http://localhost:3000/img/background_4.jpg', alt: 'img4', urlTo: '/product'},
-  {id: 5, imgUrl: 'http://localhost:3000/img/background_5.jpg', alt: 'img5', urlTo: '/product'},
+  {id: 1, imgUrl: 'http://localhost:3000/img/background_1.jpg', alt: 'img1', urlTo: '/product1'},
+  {id: 2, imgUrl: 'http://localhost:3000/img/background_2.jpg', alt: 'img2', urlTo: '/product2'},
+  {id: 3, imgUrl: 'http://localhost:3000/img/background_3.jpg', alt: 'img3', urlTo: '/product3'},
+  {id: 4, imgUrl: 'http://localhost:3000/img/background_4.jpg', alt: 'img4', urlTo: '/product4'},
+  {id: 5, imgUrl: 'http://localhost:3000/img/background_5.jpg', alt: 'img5', urlTo: '/product5'},
 ]
 
 const Slider: React.FC = () => {
@@ -37,14 +37,18 @@ const Slider: React.FC = () => {
       loop
       speed={800}
       className={s.swiper__wrapper}
-      autoplay={ {delay: 2000, pauseOnMouseEnter: true, disableOnInteraction: false} }
+      autoplay={ {delay: 6000, pauseOnMouseEnter: true, disableOnInteraction: false} }
       allowTouchMove
+      slideToClickedSlide
+      url={'/local'}
     >
       
         {slideImages.map((slide, index) => {
           return (
             <SwiperSlide>
-              <img className={s.slide__img} src={slide.imgUrl} alt={slide.alt} />
+              <a className={s.slide__link} href={slide.urlTo}>
+                <img className={s.slide__img} src={slide.imgUrl} alt={slide.alt} />
+              </a>
             </SwiperSlide>
           )
         })}
