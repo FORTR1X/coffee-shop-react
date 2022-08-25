afeec9500bd83a9e5dea71183f99c7bff011749b
FORTRIX <fortrix07@gmail.com>
2022-08-25 17:40:08 +0300

    Add layout modal product

--- src/components/Product/ModalProduct/ModalProduct.tsx
+++ src/components/Product/ModalProduct/ModalProduct.tsx
@@ -14,14 +14,22 @@ type ModalProductType = {
 }
 
 const ModalProduct: React.FC<ModalProductType> = (props: ModalProductType) => {
+
   const [productCount, setProductCount] = useState(1)
   const handleMinusProductCount = () => {
-    if (productCount > 2)
+    if (productCount > 1)
       setProductCount(productCount - 1)
   }
   
   const handlePlusProductCount = () => {
     setProductCount(productCount + 1)
+  }
+
+  const handleSetProductCount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const productCount = Number(event.currentTarget.value)
+
+    if (productCount > 0)
+      setProductCount(productCount)
   }
 
   return (
@@ -48,16 +56,35 @@ const ModalProduct: React.FC<ModalProductType> = (props: ModalProductType) => {
               <div onClick={() => {props.handleClose(false)}} className={s.header__close}/>
             </div>  
 
-            <span>{props.product.price} руб
-              <span>
-                {props.product.subcategory.category.title == 'Чай' ? '/50 гр' : '/1 шт'}
-              </span>
-            </span>
+            <div></div>
 
-            <div className={s.counter__container}>
-              <span onClick={handleMinusProductCount} className={s.counter__minus}>-</span>
-              <span className={s.counter__count}>{productCount}</span>
-              <span onClick={handlePlusProductCount} className={s.counter__plus}>+</span>
+            <div className={s.product__content}>
+              <div className={s.price}>  
+                <span>{props.product.price} руб
+                  <span className={s.price__unit}>
+                    {props.product.subcategory.category.title == 'Чай' ? '/50 гр' : '/1 шт'}
+                  </span>
+                </span>
+              </div>
+
+              <div className={s.btn__group}>
+                <div className={s.counter__container}>
+                  <input type='button' onClick={handleMinusProductCount} className={s.counter__minus} value="-"/>
+                  <input type='text' onChange={(event) => {handleSetProductCount(event)}} className={s.counter__count} value={productCount}/>
+                  <input type='button' onClick={handlePlusProductCount} className={s.counter__plus} value="+"/>
+                </div>
+
+                <div className={s.btn__add_to_cart}>
+                  В корзину
+                </div>
+              </div>
+
+              <span className={s.description}>{props.product.description}</span>
+
+              <div className={s.split_line}/>
+
+              <span className={s.product_id}>Артикул: <span>#{props.product.id}</span></span>
+              <span className={s.product_category}>Категория: <span>{props.product.subcategory.category.title}, {props.product.subcategory.title}</span></span>
             </div>
           </div>
         </div>
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -35,8 +35,6 @@ const Product: React.FC<ProductPropsType> = (props: ProductPropsType) => {
 
   useEffect(() => {
     document.body.classList.toggle(s.body__no_scroll)
-    if (isProductModalOpen && productHoveredRef.current !== null)
-      productHoveredRef.current.className = s.product__hovered_container
   }, [isProductModalOpen])
 
   const teaUnits: Array<TeaUnitsType> = [
@@ -49,33 +47,6 @@ const Product: React.FC<ProductPropsType> = (props: ProductPropsType) => {
   ].reverse()
 
   const productRef = useRef<HTMLDivElement>(null);
-  const productHoveredRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseEnterAnimation = (): void => {
-    if (productRef.current !== null && productHoveredRef.current !== null) {
-      productRef.current.className = `${s.product} ${s.open}`
-      setTimeout(() => {
-        if (productHoveredRef.current !== null) {
-          productHoveredRef.current.className = `${s.product__hovered_container} ${s.start}`
-        }
-      }, 300)
-    }
-  }
-
-  const handleMouseLeaveAnimation = (): void => {
-    if (productRef.current !== null && productHoveredRef.current !== null) {
-      productHoveredRef.current.className = `${s.product__hovered_container} ${s.end}`
-      setTimeout( () => {
-        if (productHoveredRef.current !== null)
-        productHoveredRef.current.className = `${s.product__hovered_container} ${s.end_active}`
-      }, 500)
-
-      setTimeout( () => {
-        if (productRef.current !== null)
-          productRef.current.className = `${s.product}`
-      }, 300)
-    }
-  }
 
   const [isTeaUnitsOpen, setIsTeaUnitsOpen] = useState(false)
   const [currentTeaUnits, setCurrentTeaUnits] = useState<TeaUnitsType>(teaUnits[5])
@@ -119,41 +90,42 @@ const Product: React.FC<ProductPropsType> = (props: ProductPropsType) => {
   return (
     <div
       ref={productRef}
-      onMouseLeave={handleMouseLeaveAnimation}
-      onMouseEnter={handleMouseEnterAnimation}
       className={s.product}>
+
+      <div className={s.product__img}>
+        <a href={`/product/id${props.product.id}`}>
+          <img 
+            className={s.img}
+            src={`http://localhost:8080/uploads/product/${props.product.id}.jpg`} 
+            alt={props.product.header} 
+          />
+        </a>  
+        <div className={s.img__hovered_content}>
+          <img
+            className={s.img__search}
+            src={searchSvg}
+            alt="view"
+            title="Просмотреть товар"
+            onClick={ () => {
+              setCurrentProduct(props.product)
+              handleSetIsProductModalOpen()
+            }}
+          />
+        </div>
+      </div>
+
       <div className={s.product__container}>
-        <div className={s.product__img}>
-          <a href={`/product/id${props.product.id}`}>
-            <img 
-              className={s.img}
-              src={`http://localhost:8080/uploads/product/${props.product.id}.jpg`} 
-              alt={props.product.header} 
-            />
-          </a>  
-          <div className={s.img__hovered_content}>
-            <img
-              className={s.img__search}
-              src={searchSvg}
-              alt="view"
-              title="Просмотреть товар"
-              onClick={ () => {
-                setCurrentProduct(props.product)
-                handleSetIsProductModalOpen()
-              }}
-            />
+        <div>
+          <div className={s.product__title}>
+            <a href={`/product/id${props.product.id}`}>
+              {props.product.header}
+            </a>
           </div>
-        </div>
 
-        <div className={s.product__title}>
-          <a href={`/product/id${props.product.id}`}>
-            {props.product.header}
-          </a>
-        </div>
-
-        <div className={s.product__category}>
-          {`${props.product.subcategory.category.title}, ${props.product.subcategory.title}`}
-        </div>
+          <div className={s.product__category}>
+            {`${props.product.subcategory.category.title}, ${props.product.subcategory.title}`}
+          </div>
+        </div>  
 
         <div className={s.product__price}>
           <span className={s.price}>
@@ -165,60 +137,7 @@ const Product: React.FC<ProductPropsType> = (props: ProductPropsType) => {
               <span className={s.count}> /1 шт</span>
             }
           </span>
-        </div>
-
-        <div ref={productHoveredRef} className={s.product__hovered_container}>
-          <div className={s.product__description}>
-            <span>{props.product.description}</span>
-          </div>  
-          
-          {/* For tea */}
-          {props.product.subcategory.category.title == 'Чай' &&
-            <div className={s.product__btn_group_tea}>
-              <ul className={s.product__select_count_tea}>
-                <li
-                 className={s.tea__li}
-                 onClick={() => handleOnClickTeaUnits()}
-                 onMouseLeave={() => {handleFadeOutTeaUnits()}}
-                >
-                  <span className={s.tea__default_value}>
-                    {currentTeaUnits.title}
-                    <span className={s.tea__arrow}/>
-                  </span>
-                  <ul ref={teaUnitsRef} className={s.tea_units__ul}>
-                    {teaUnits.map((unit, index) => {
-                      return (
-                        <li 
-                          key={(index + 1) * 4}
-                          className={s.tea_units__li}
-                          onClick={() => handleSetCurrentTeaUnits(unit)}
-                        >
-                          {unit.title}
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </li>
-                
-              </ul>
-
-              <div>
-                <div className={s.cart_tea}>
-                  <img src={cartSvg} alt="cart" title="Добавить товар"/>
-                  <span>{(currentTeaUnits.id == null ? 1 : currentTeaUnits.id) * props.product.price} руб</span>
-                </div>
-              </div>
-            </div>  
-          }
-
-          {/* Everything but tea */}
-          {props.product.subcategory.category.title !== 'Чай' &&
-            <div className={s.cart}>
-              <img src={cartSvg} alt="cart" title="Добавить товар"/>
-              <span className={s.cart__btn}>В корзину</span>
-            </div>
-          }
-        </div>
+        </div> 
       </div>
 
       {currentProduct !== undefined && isProductModalOpen &&
